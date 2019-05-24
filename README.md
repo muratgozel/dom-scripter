0f836ee06db7b1f98aa2fc97fc20fb06fb10dc3f
Murat Gözel <murat@gozel.com.tr>
2019-05-25 02:15:55 +0300

    Initial release.

--- README.md
+++ README.md
@@ -1,1 +1,95 @@
-# dom-scripter
+# DOM Script Injector
+
+Easily inject script tags and jsonld documents into your html document with this javascript module.
+
+## Install
+
+```sh
+npm install dom-scripter
+```
+
+## Import
+
+The default file imported by the module is polyfilled, bundled `dist/index.js` file:
+
+```js
+const scripter = require('dom-scripter')
+```
+
+If you already have a compiler like babel or already polyfilling your codebase, using `./source/index.js` recommended.
+
+```js
+const scripter = require('dom-scripter/source')
+```
+
+## Use
+
+The module exposes 3 API methods for user.
+
+### Inject script
+
+```js
+scripter.inject('/path/to/script.js', {id: 'identifier'})
+```
+
+Output inside the html document will be:
+
+```html
+<script type="text/javascript" src="/path/to/script.js" id="identifier" async></script>
+```
+
+With all available options:
+
+```js
+scripter.inject('/path/to/script.js', {
+  id: 'identifier',
+  type: 'text/javascript',
+  async: false,
+  attrs: {
+    'data-hey': 'hello'
+  }
+})
+```
+
+Output inside the html document will be:
+
+```html
+<script type="text/javascript" src="/path/to/script.js" id="identifier" data-hey="hello"></script>
+```
+
+### Inject JSON-LD Document
+
+JSON-LD documents are used commonly to inject [**Google's Structured Data**][b7dd4d5a] for search engine optimization.
+
+  [b7dd4d5a]: https://developers.google.com/search/docs/guides/intro-structured-data "Google's Structured Data"
+
+```js
+const doc = {
+  "@type":"BreadcrumbList",
+  "itemListElement": [
+    {
+      "@type": "ListItem",
+      "position": 1,
+      "name": "Home",
+      "item": "https://example.com"
+    }
+  ]
+}
+scripter.injectJSONLD(doc, {id: 'identifier'})
+```
+
+Output inside the html document will be:
+
+```html
+<script type="application/ld+json" id="identifier">{"@type":"BreadcrumbList","itemListElement":[{"@type":"ListItem","position":1,"name":"Home","item":"https://example.com"}]}</script>
+```
+
+Properties and values placed inside the `attrs` property in options will be added to the `script` tag automatically like in the `inject` method.
+
+### Remove Element
+
+Removes injected element from DOM.
+
+```js
+scripter.remove('identifier')
+```
---
+++ source/index.js
@@ -0,0 +1,104 @@
+function Scripter() {
+  this.processingIds = []
+  this.timeoutInterval = 7000
+}
+
+Scripter.prototype.inject = function inject(url, opts = {}) {
+  const self = this
+
+  return new Promise(function(resolve, reject) {
+    // return existing scripts
+    if (typeof opts.id == 'string') {
+      if (document.getElementById(opts.id)) {
+        return resolve(opts.id)
+      }
+    }
+
+    if (typeof opts.id == 'string' && self.processingIds.indexOf(opts.id) !== -1) {
+      const err = new Error('Script already being injected. (' + opts.id + ')')
+      return reject(err)
+    }
+
+    // register timeout
+    let t = setTimeout(function() {
+      const err = new Error('Script injection timeout. (' + url + ')')
+      return reject(err)
+    }, self.timeoutInterval)
+
+    // create
+    function onDone() {
+      clearTimeout(t)
+      return resolve(opts.id)
+    }
+
+    function onError(err) {
+      clearTimeout(t)
+      return reject(err)
+    }
+
+    const s = document.createElement('script')
+
+    s.type = opts.type || 'text/javascript'
+    s.async = opts.hasOwnProperty('async') ? opts.async : true
+    if (typeof opts.id == 'string') s.id = opts.id
+    if (opts.attrs) {
+      Object.keys(opts.attrs).map(function(name) {
+        s.setAttribute(name, opts.attrs[name])
+      })
+    }
+
+    if (s.addEventListener) {
+      s.addEventListener('load', onDone, false)
+    }
+    else if (s.readyState) {
+      s.onreadystatechange = onDone
+    }
+    else if (s.onload) {
+      s.onload = onDone
+    }
+
+    if (s.onerror) {
+      s.onerror = onErr
+    }
+
+    s.src = url
+
+    let elems = document.getElementsByTagName('script')
+    if (!elems || elems.length < 1) elems = document.getElementsByTagName('head')
+    const ref = elems[0]
+    ref.parentNode.insertBefore(s, ref)
+    elems = null
+  })
+}
+
+Scripter.prototype.injectJSONLD = function injectJSONLD(input, opts = {}) {
+  // remove existing scripts
+  if (typeof opts.id == 'string') {
+    const es = document.getElementById(opts.id)
+    if (es) es.parentNode.removeChild(es)
+  }
+
+  const s = document.createElement('script')
+
+  s.type = 'application/ld+json'
+  if (typeof opts.id == 'string') s.id = opts.id
+  if (opts.attrs) {
+    Object.keys(opts.attrs).map(function(name) {
+      s.setAttribute(name, opts.attrs[name])
+    })
+  }
+  s.text = typeof input == 'string' ? input : JSON.stringify(input)
+
+  let elems = document.getElementsByTagName('script')
+  if (!elems || elems.length < 1) elems = document.getElementsByTagName('head')
+  const ref = elems[0]
+  ref.parentNode.insertBefore(s, ref)
+  elems = null
+}
+
+Scripter.prototype.remove = function remove(opts = {}) {
+  const s = document.getElementById(opts.id || '')
+  if (s) s.parentNode.removeChild(s)
+}
+
+module.exports = new Scripter()
